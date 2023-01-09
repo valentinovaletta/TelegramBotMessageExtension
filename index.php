1cf2d0d631de8db5b672f1b0d81f97e9952321e8
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2023-01-09 12:10:09 +0200

    init

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$log = new Monolog\Logger('name');
+$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+$log->addWarning('add Warning');
