38817fcb55c9a42cdb0a338ee5c37b6b2400c62e
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2023-05-16 16:44:08 +0300

    token hide

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ require_once('src/TelegramBotMessageExtension/Message.php');
 
 use TelegramBotMessageExtension\Message;
 
-$id = 494963311;
-$token = '5111024503:AAFhDgDyZ3n1hgHKPtfXb8qlZDacVhRBVJ0';
+$id = 0; // telegram channel id
+$token = ''; // telegram bot token 
 
 $message = new Message($token, $id);
 
