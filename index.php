063ec89d9a3f8f51bcbd465ac8df73dd233d7e71
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2023-05-16 16:37:25 +0300

    Telegram bot message extension

--- index.php
+++ index.php
@@ -1,10 +1,44 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
+require_once('src/TelegramBotMessageExtension/Message.php');
 
-$log = new Monolog\Logger('name');
-$name = new TestLib\TestClass\User('Ivan4');
+use TelegramBotMessageExtension\Message;
 
+$id = 494963311;
+$token = '5111024503:AAFhDgDyZ3n1hgHKPtfXb8qlZDacVhRBVJ0';
 
-$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
-$log->Warning( $name->getName() );
+$message = new Message($token, $id);
+
+//send single text message
+$singleMesage = $message->sendSingleMessage("<b>Hi!</b>\n<i>There</i>\n", "HTML");
+
+//send sticker
+$sticker = $message->sendSticker('CAACAgIAAxkBAAETkGNiblYnE7xLE5GJ1kABRAaf3WTd4QACrxQAAs7y2UmyHBuHGFJROCQE');
+
+//send three messages that replace each other
+//parameter must be an array of arrays, that describes each message. [[],[],[]]
+$queue = $message->sendMessagesQueue([
+    [
+        'method' => 'sendMessage',
+        'delay'  => 1000000,
+        'param' => [
+            'text' => '<i>Hello!</i>',
+            'parse_mode' => 'HTML'
+        ]
+    ],
+    [        
+        'method' => 'sendSticker',
+        'delay'  => 1000000,
+        'param' => [
+            'sticker' => 'CAACAgIAAxkBAAETkGNiblYnE7xLE5GJ1kABRAaf3WTd4QACrxQAAs7y2UmyHBuHGFJROCQE'
+        ]
+    ],
+    [
+        'method' => 'sendMessage',
+        'delay'  => 1000000,
+        'param' => [
+            'text' => '<i>End!</i>',
+            'parse_mode' => 'HTML'
+        ]
+    ]
+]);
---
+++ src/TelegramBotMessageExtension/Message.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace TelegramBotMessageExtension;
+
+class Message
+{
+    private $token;
+    private $id;
+
+    public function __construct($token, $id)
+    {
+        $this->token = $token;
+        $this->id = $id;
+    }
+
+    public function sendSingleMessage($text, $mode)
+    {
+        return $this->sendRequest('sendMessage', ['chat_id' => $this->id, 'text' => $text, "parse_mode" => $mode]);
+    }
+
+    public function sendSticker($sticker)
+    {
+        return $this->sendRequest('sendSticker', ['chat_id' => $this->id, 'sticker' => $sticker]);
+    }
+
+    public function sendMessagesQueue($messagesArray)
+    {
+        $i = 1;
+        foreach($messagesArray as $message){
+            $message['param']['chat_id'] = $this->id;
+            $request = $this->sendRequest($message['method'], $message['param']);
+            usleep($message['delay']);
+            if($i++ < count($messagesArray)){
+                $this->sendRequest('deleteMessage', ['chat_id' => $this->id, 'message_id' => $request->result->message_id]);
+            }
+        }
+    }
+
+    private function sendRequest($method, $param=[]) {
+        $url = "https://api.telegram.org/bot$this->token/$method?";
+        echo $url .= http_build_query($param);
+        $ch = curl_init();
+        $optArray = array(
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true
+        );
+        curl_setopt_array($ch, $optArray);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($result);
+    }   
+
+}
